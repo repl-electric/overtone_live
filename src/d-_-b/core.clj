4324f1e9f2e19c7e6b097bd297ff45191ab0b9e7
Joseph Wilk <joe@josephwilk.net>
2016-08-17 12:57:36 +0100

    new ascii

--- src/d-_-b/core.clj
+++ src/d-_-b/core.clj
@@ -1,11 +1,6 @@
-(ns d-_-b.core
-" __                             __
- /\ \                           /\ \
- \_\ \                          \ \ \____
- /'_` \  _______          _______\ \ '__`\
-/\ \L\ \/\______\        /\______\\ \ \L\ \
-\ \___,_\/______/        \/______/ \ \_,__/
- \/__,_ /          _______          \/___/
-                  /\______\
-                  \/______/"
-(:use [overtone.live] [mud.core])(:require [mud.timing :as time] [overtone.studio.fx :as fx]  [shadertone.tone :as t]))
+(ns d-_-b.core "_               _
+              _| |___       ___| |_
+             | . |___|     |___| . |
+             |___|    _____    |___|
+                     |_____|"
+(:use [overtone.live] [mud.core])(:require [mud.timing :as time][overtone.studio.fx :as fx][shadertone.tone :as t]))
